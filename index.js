a6ee09fd5a8a8cd0c362c73b5e01775f1113766d
UniqueDevStorm <storm@uniquecode.team>
2021-03-08 19:18:44 +0900

    feat: Google OAuth2

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const port = 3000;
 
 const KEY = process.env.API_KEY;
 const API_BASE_URI = {
-    Kakao : 'https://kauth.kakao.com'
+    Kakao : 'https://kauth.kakao.com',
+    Google : 'https://accounts.google.com/o'
 }
 
 const REDIRECT_URI = {
-    Kakao : 'http://localhost:3000/api/oauth2/kakao/callback'
+    Kakao : 'http://localhost:3000/api/oauth2/kakao/callback',
+    Google : 'http://localhost:3000/api/oauth2/google/callback'
 }
 
 const API_KEY = {
-    Kakao : process.env.KAKAO_API_KEY
+    Kakao : process.env.KAKAO_API_KEY,
+    Google : {
+        CLIENT_ID : process.env.GOOGLE_API_CLIENT_ID,
+        CLIENT_SECRET : process.env.GOOGLE_API_CLIENT_SECRET
+    }
 }
 
 app.get('/api/oauth2/kakao', (req, res) => {
@@ -41,6 +47,34 @@ app.get('/api/oauth2/kakao/callback', async (req, res) => {
         }
     })).json())
     res.send(user)
+})
+
+app.get('/api/oauth2/google', (req, res) => {
+    res.redirect(`https://accounts.google.com/o/oauth2/auth?client_id=${API_KEY.Google.CLIENT_ID}&redirect_uri=${REDIRECT_URI.Google}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/userinfo.profile`)
+})
+
+app.get('/api/oauth2/google/callback', async (req, res) => {
+    const code = req.query.code;
+    const params = new URLSearchParams({
+        client_id: API_KEY.Google.CLIENT_ID,
+        client_secret: API_KEY.Google.CLIENT_SECRET,
+        code: code,
+        redirect_uri: REDIRECT_URI.Google,
+        grant_type: 'authorization_code'
+    })
+    const request = (await (await fetch('https://oauth2.googleapis.com/token', {
+        method: 'POST',
+        body: params,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })).json())
+    const access_token = request.access_token;
+    const user = (await (await fetch(`https://www.googleapis.com/oauth2/v3/userinfo`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${access_token}`
+        }
+    })).json())
+    res.send(user);
 })
 
 app.listen(port, () => {
